1aad4ab5b2f2d6028b2bf98058afa1d4b07bf52b
Jay002 <jayesh2598@gmail.com>
2020-09-30 20:25:32 +0530

    Added: First name is validated

--- main/java/com/capgemini/registration/Main.java
+++ main/java/com/capgemini/registration/Main.java
@@ -1,9 +1,21 @@
 package com.capgemini.registration;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	
+	static Scanner sc=new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration!");
+		
+		System.out.println("Enter the first name:");
+		String firstName = sc.nextLine().trim();
+		
+		if(firstName.matches("^[A-Z][a-z]{2,}")) 
+			System.out.println("First Name is valid.");
+		else
+			System.out.println("First Name is invalid.");
 	}
 
 }
