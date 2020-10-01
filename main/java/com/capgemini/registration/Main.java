d78a1c9f7f5bc8ec71bdddef9c7fe0956e552b8d
Jay002 <jayesh2598@gmail.com>
2020-10-01 10:17:10 +0530

    Added: Mobile number is validated

--- main/java/com/capgemini/registration/Main.java
+++ main/java/com/capgemini/registration/Main.java
@@ -12,6 +12,7 @@ public class Main {
 		validate("First Name","^[A-Z][a-z]{2,}");
 		validate("Last Name", "^[A-Z][a-z]{2,}");
 		validate("Email","(^[a-zA-Z][a-zA-Z0-9_]{2,})([.+-][a-zA-Z0-9]{1,})?@([a-zA-Z0-9]{1,}).[a-zA-Z]{2,}(.[a-zA-Z]{2,})$");
+		validate("Mobile number","[0-9]{1,2}[' '][7-9][0-9]{9}");
 		System.out.println("Registration successful!");
 	}
 			
