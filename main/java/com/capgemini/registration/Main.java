207c8b2ae64cdbe3d5eca0be0f695f58f7372c5d
Jay002 <jayesh2598@gmail.com>
2020-10-01 10:40:35 +0530

    Added: Validated password for atleast one uppercase and number and minimum 8 characters

--- main/java/com/capgemini/registration/Main.java
+++ main/java/com/capgemini/registration/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		validate("Last Name", "^[A-Z][a-z]{2,}");
 		validate("Email","(^[a-zA-Z][a-zA-Z0-9_]{2,})([.+-][a-zA-Z0-9]{1,})?@([a-zA-Z0-9]{1,}).[a-zA-Z]{2,}(.[a-zA-Z]{2,})$");
 		validate("Mobile number","[0-9]{1,2}[' '][7-9][0-9]{9}");
-		validate("Password","(?=.*[A-Z]).{8,}");
+		validate("Password","(?=.*[A-Z])(?=.*[0-9]).{8,}");
 		System.out.println("Registration successful!");
 	}
 			
